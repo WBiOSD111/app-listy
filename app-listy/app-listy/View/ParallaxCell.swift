224ecc14c75ff91151d2422c94431000856892f5
grav0211 <grav0211@algonquinlive.com>
2018-11-11 15:23:41 -0500

    Added custom parallax effect code

--- app-listy/app-listy/View/ParallaxCell.swift
+++ app-listy/app-listy/View/ParallaxCell.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ParallaxCell: UITableViewCell {
     
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var itemDescription: UILabel!
+    
+    var motionManager: CMMotionManager!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -27,18 +30,60 @@ class ParallaxCell: UITableViewCell {
         let min = CGFloat(-30)
         let max = CGFloat(30)
         
-        let xMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
-        xMotion.minimumRelativeValue = min
-        xMotion.maximumRelativeValue = max
+        let trueXCenter = itemImageView.center.x
+        let trueYCenter = itemImageView.center.y
+
+        // Custom Parallax Effect
         
-        let yMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.y", type: .tiltAlongVerticalAxis)
-        yMotion.minimumRelativeValue = min
-        yMotion.maximumRelativeValue = max
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main) { (data, error) in
+            guard let accelerometerData = data else { return }
+            
+            var x: CGFloat = 0.0
+            var y: CGFloat = 0.0
+            
+            if (self.itemImageView.center.x) <= (trueXCenter + max) && (self.itemImageView.center.x) >= (trueXCenter + min) {
+                x = trueXCenter + CGFloat(30*(accelerometerData.acceleration.x))
+            } else if (self.itemImageView.center.x) > (trueXCenter + max)  {
+                self.itemImageView.center.x = trueXCenter + max
+                return
+            } else {
+                self.itemImageView.center.x = trueXCenter + min
+                return
+            }
+            
+            if (self.itemImageView.center.y) <= (trueYCenter + max) && (self.itemImageView.center.y) >= (trueYCenter + min) {
+                y = trueYCenter + CGFloat(30*(accelerometerData.acceleration.y))
+            } else if (self.itemImageView.center.y) > (trueYCenter + max)  {
+                self.itemImageView.center.y = trueYCenter + max
+                return
+            } else {
+                self.itemImageView.center.y = trueYCenter + min
+                return
+            }
+            
+            print("X: \(x) Y: \(y)")
+            
+            self.itemImageView.center.x = x
+            self.itemImageView.center.y = y
+            
+        }
         
-        let motionEffectGroup = UIMotionEffectGroup()
-        motionEffectGroup.motionEffects = [xMotion, yMotion]
+        // Parallax Effect using UIInterpolatingMotionEffect
         
-        itemImageView.addMotionEffect(motionEffectGroup)
+//        let xMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
+//        xMotion.minimumRelativeValue = min
+//        xMotion.maximumRelativeValue = max
+//
+//        let yMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.y", type: .tiltAlongVerticalAxis)
+//        yMotion.minimumRelativeValue = min
+//        yMotion.maximumRelativeValue = max
+//
+//        let motionEffectGroup = UIMotionEffectGroup()
+//        motionEffectGroup.motionEffects = [xMotion, yMotion]
+//
+//        itemImageView.addMotionEffect(motionEffectGroup)
+        
     }
 
 }
